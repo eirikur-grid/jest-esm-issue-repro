f4087b51a3e157a720f91874f8bbf5037b389240
Eiríkur Fannar Torfason <eirikur@grid.is>
2023-01-09 11:14:32 +0000

    Simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,5 @@
 module.exports = {
-  assumptions: {
-    privateFieldsAsProperties: true,
-    setPublicClassFields: true,
-  },
   presets: [
-    [
-      'next/babel',
-      {
-        'class-properties': {},
-      },
-    ],
-  ],
-  plugins: [
-    [ '@babel/plugin-proposal-export-default-from' ],
-    [ '@babel/plugin-proposal-private-methods' ],
-    [
-      'module-resolver',
-      {
-        root: [ './src' ],
-      },
-    ],
+    [ 'next/babel' ],
   ],
 };
